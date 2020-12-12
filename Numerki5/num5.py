9816e7c5e02ee8823c3ed022a14d221a677fec39
Brandon Swatek <bswatekpost@gmail.com>
2020-12-12 14:43:06 +0100

    Ex 5 redone

--- Numerki5/num5.py
+++ Numerki5/num5.py
@@ -6,60 +6,44 @@ def create_a(n):
     for i in range(n):
         for j in range(n):
             if i == j:
-                matrix_a[i][j] = 5
+                matrix_a[i][j] = 4
             elif (i - j) == 1 or (j - i) == 1 or (j - i) == 4 or (i - j) == 4:
-                matrix_a[i][j] = 2
-            else:
                 matrix_a[i][j] = 1
+            else:
+                matrix_a[i][j] = 0
     return matrix_a
 
 
-def thomas(a, b, c, d):
-    nf = len(d)
-    ac, bc, cc, dc = map(np.array, (a, b, c, d))
-    for it in range(1, nf):
-        mc = ac[it - 1] / bc[it - 1]
-        bc[it] = bc[it] - mc * cc[it - 1]
-    dc[it] = dc[it] - mc * dc[it - 1]
-    xc = bc
-    xc[-1] = dc[-1] / bc[-1]
-    for il in range(nf - 2, -1, -1):
-        xc[il] = (dc[il] - cc[il] * xc[il + 1]) / bc[il]
-    return xc
-
-
-def preconditioner(matrix_a):
-    n = len(matrix_a[0])
-    jacobi = np.zeros((n, n))
-    diag = np.diagonal(matrix_a)
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                jacobi[i][j] = diag[i]
-    return jacobi
-
-
-def preconditioned_conjugate_gradients(matrix_a, vector_x, vector_b, iterations):
-    r = vector_b - np.dot(matrix_a, vector_x)
-    matrix_m = preconditioner(matrix_a)
-    z = thomas(matrix_m.diagonal(-1), matrix_m.diagonal(), matrix_m.diagonal(1), r)
-    p = z
-    rsold = np.dot(r.T, z)
+def gauss_seidel(matrix_a, vector_x, vector_b, iterations):
+    n = len(matrix_a)
     for k in range(iterations):
-        Ap = np.dot(matrix_a, p)
-        alpha = float(rsold / np.dot(np.transpose(p), Ap))
-        vector_x = vector_x + np.dot(alpha, p)
-        r = r - np.dot(alpha, Ap)
-        z = thomas(matrix_m.diagonal(-1), matrix_m.diagonal(), matrix_m.diagonal(1), r)
-        rsnew = np.dot(np.transpose(r), z)
-        p = z + (rsnew / rsold) * p
-        rsold = rsnew
+        for i in range(n):
+            temp = vector_b[i]
+            for j in range(n):
+                if i != j and matrix_a[i][j] != 0:
+                    temp -= matrix_a[i][j] * vector_x[j]
+            vector_x[i] = temp / matrix_a[i][i]
     return vector_x
 
 
+def sherman_morrison(matrix_a, martix_u, matrix_v, vector_b):
+    vector_u = np.array(martix_u[:, :1])
+    vector_vt = np.array(matrix_v.T[0])
+    vector_z = gauss_seidel(np.copy(matrix_a), np.zeros(len(vector_vt)), np.copy(vector_b), 42)
+    vector_q = gauss_seidel(np.copy(matrix_a), np.zeros(len(vector_vt)), np.copy(vector_u.T[0]), 42)
+    return vector_z - vector_q * (np.dot(vector_vt, vector_z) / (1 + np.dot(vector_vt, vector_q)))
+
+
 if __name__ == '__main__':
+    # Ax = e
     e = np.ones(64)
-    x = np.zeros(64)
     A = create_a(64)
+    u = np.zeros((64, 64))
+    u[:, 0] = 1
+    v = np.copy(u)
+
+    print(sherman_morrison(A, u, v, e))
+
+    A = np.array(A+(u@v.T))
     print(np.linalg.solve(A, e))
-    print(preconditioned_conjugate_gradients(A, x, e, 5))
+
